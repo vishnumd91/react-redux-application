1895a9e009564554e40d7a0bca8d68f83447a49a
Vishnu MD <vishnudivakaran1@gmail.com>
2021-09-10 19:49:16 +0530

    Initial App Component Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-export default class App extends PureComponent {
-  render() {
-    return (
-      <div>
-        <h1>Hello from React</h1>
-      </div>
-    );
-  }
-}
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import './index.css';
+
+import AboutPage from './components/about/AboutPage';
+import HomePage from './components/home/HomePage';
+import Header from './components/common/Header';
+import PageNotFound from './components/404/PageNotFound';
+import Courses from './components/courses/Courses';
+
+const App = () => (
+  <div className="container-fluid">
+    <Header />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/courses" component={Courses} />
+      <Route component={PageNotFound} />
+    </Switch>
+  </div>
+);
+
+export default App;
---
+++ src/components/courses/Courses.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Courses extends Component {
+  render() {
+    return (
+      <div>
+        Courses
+      </div>
+    );
+  }
+}
